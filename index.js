344135ca0844f6ec8358ffe2a8f9dbcb8ff108fe
Julian Ward <wardjulianm@gmail.com>
2023-03-01 10:44:41 -0700

    I was coded

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// Flexible function
+// function addingEventListener(idString) {
+//   
+//    function clickAlert() {
+//        alert('I was clicked!');
+//      }
+//    
+//    const input = document.getElementById(idString)
+//
+//
+//    input.addEventListener('click', clickAlert);
+//}
+
 function addingEventListener() {
-}
+       function clickAlert() {
+           alert('I was clicked!');
+          }
+        
+        const input = document.getElementById('button')
+    
+    
+        input.addEventListener('click', clickAlert);
+    }
